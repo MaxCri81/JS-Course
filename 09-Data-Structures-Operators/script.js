3dcc8bc2fc225fdef9c350edc497f2163a1f2e89
Massimiliano Crisafulli <maxone981@gmail.com>
2025-07-04 18:51:26 +0100

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,9 +1,6 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const italianFoods = new Set([
   'pasta',
   'gnocchi',
@@ -1479,3 +1476,23 @@ function camelCase()
     console.log(output.padEnd(20), "✅".repeat(index + 1));
   }  
 }
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+// Formatting flights as in the example above.
+// Return the first 3 digits of the string in upper cases
+/*
+const getCode = value => value.toUpperCase().slice(0, 3);
+
+for (const flight of flights.split("+"))
+{
+  const [type, from, to, time] = flight.split(";");
+  const output = `${type.startsWith("_Delayed") ? "🔴 " : ""}${type.slice(1).replace("_", " ")} from ${getCode(from)} to ${(getCode(to))} (${time.replace(time[2], "h")})`.padStart(45, " ");
+  console.log(output);
+}
+*/
