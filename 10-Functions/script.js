c901d64bd76a7fccbca8f64abb9a9b18593bd6d8
Massimiliano Crisafulli <maxone981@gmail.com>
2025-07-05 13:09:41 +0100

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,23 @@
 'use strict';
+/************************************************************************** Defaul values *****************************************************************************/
+const bookingsArray = [];
+/**
+ * Store bookings in an array.
+ * @param {string} flightNum number of the flight
+ * @param {number} numPassengers number of passengers, default value 1
+ * @param {number} price of the flight, default value 199 * number of passengers
+ */
+function createBooking(flightNum, numPassengers = 1, price = 199 * numPassengers)
+{
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    };
+    console.log(booking);
+    bookingsArray.push(booking);
+};
+createBooking("A920");
+createBooking("A230", 2);
+createBooking("A340", 2, 500);
+createBooking("A340", undefined, 270); // skip the second parameter, leaving it blank
