384bbff47c87229db57a20a1d364eb73eb6cb6a9
Massimiliano Crisafulli <maxone981@gmail.com>
2025-07-06 09:24:58 +0100

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -73,4 +73,47 @@ greet("Hi")("Giusy");
 const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
 greetArrow("Hi")("Max");
 greetArrow("Hi")("Giusy");
-*/
+
+/***************************************************************************** Call and Apply method ******************************************************************/
+const ryanair = {
+    airline: "Ryanair",
+    iatacode: "RY",
+    bookings: [],
+    //book: function(flightNum, name){} traditional declaration inside object
+    book(flightNum, name){ //enhanced function declaration inside object
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iatacode}${flightNum}`);
+        this.bookings.push({flight: `${this.iatacode}${flightNum}`, name}); //we don't need to write name: name
+    }
+}
+ryanair.book(239, "Massimo Crisafulli");
+ryanair.book(312, "Mike Smith");
+console.log(ryanair.bookings);
+
+const bookFunction = ryanair.book;
+
+const easyjet = {
+    airline: "Easyjet",
+    iatacode: "EA",
+    bookings: []
+};
+
+//It doesn't work because 'this' points to the wrong object, in this scenario. 
+// The method is copied outside the object and when it is called, 'this' points to undefined,
+// so, the caller has changed.
+//bookFunction(23, "Sarah Williams"); 
+// instead we can use 'call' or 'apply'.
+/*
+bookFunction.call(easyjet, 23, "Sarah Williams"); // 'call' method for functions - allows you to specify where the 'this' must point to. (1st parameter)
+console.log(easyjet.bookings);
+
+bookFunction.call(ryanair, 467, "Andrew Cooper");
+console.log(ryanair.bookings);
+
+bookFunction.apply(easyjet, [542, "Andrew Red"]);
+console.log(easyjet.bookings);
+
+bookFunction.call(easyjet, ...[521, "Annah Red"]); // same as the above
+console.log(easyjet.bookings);
+/*************************************************************************** Bind method ************************************************************************/
+
+
