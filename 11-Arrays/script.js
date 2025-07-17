8d09384ea3fbf916798235ce697053cbf55affc4
Massimiliano Crisafulli <maxone981@gmail.com>
2025-07-17 15:32:28 +0100

    Update script.js
    Array grouping

--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -5,6 +5,7 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  type: "premium",
 };
 
 const account2 = {
@@ -12,6 +13,7 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  type: "standard",
 };
 
 const account3 = {
@@ -19,6 +21,7 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  type: "premium",
 };
 
 const account4 = {
@@ -26,6 +29,7 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  type: "basic",
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -373,4 +377,25 @@ console.log(movements);
 // the above can be rewritten as the following
 movements.sort((a, b) => a - b);
 console.log(movements);
+/*********************************************************************** Array grouping *****************************************************************************/
+/*
+// grouping by specifying the key
+console.log(movements);
+const groupedMovements = Object.groupBy(movements, mov => mov > 0 ? "deposit" : "withdrawal");
+console.log(groupedMovements);
+
+const groupedByActivity = Object.groupBy(accounts, account => {
+  const movementsLength = account.movements.length;
+  if (movementsLength >= 8) return "very active";
+  if (movementsLength >= 5) return "active";
+  if (movementsLength >= 4) return "moderate";
+  return "inactive" 
+});
+console.log(groupedByActivity);
+
+// grouping by the key coming from the object key property
+// const groupedAccount = Object.groupBy(accounts, account => account.type);
+const groupedAccount = Object.groupBy(accounts, ({type}) => type); // with object destructuring
+console.log(groupedAccount);
 */
+
