977a587c159d37720dffe23ba85f01115ece5ca6
Massimiliano Crisafulli <maxone981@gmail.com>
2025-07-19 08:15:23 +0100

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -248,6 +248,34 @@ btnSort.addEventListener('click', function (e) {
   sorted = !sorted;
 });
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+/********************************************************************* Numbers *************************************************************************************/
+/*
+console.log(23 === 23.0); // same data type for numbers and floats
+console.log(0.1 + 0.2 === 0.3); // false because of binary conversion. In JS this cannot be compared like with decimal numbers
+// Type conversion
+console.log(Number("23"));
+console.log(+"23"); // Type coercion
+// Parsing
+console.log(Number.parseInt("30px", 10)); // base 10 decimal numbers
+console.log(Number.parseInt("ee30px", 2)); // base 2 binary numbers
+console.log(Number.parseInt(" 2.5rem "));
+console.log(Number.parseFloat(" 2.5rem "));
+console.log(parseFloat(" 2.5rem ")); // you can call it in this way too, since the method is a global variable
+
+// Check for NaN type
+console.log(Number.isNaN(20));
+console.log(Number.isNaN("20"));
+console.log(Number.isNaN(+"20px"));
+console.log(Number.isNaN(20 / 0));
+
+// Check for number type
+console.log(Number.isFinite(20));
+console.log(Number.isFinite("20"));
+console.log(Number.isFinite(+"20px"));
+console.log(Number.isFinite(20 / 0));
+
+// Check for integer type
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23/0));
+/****************************************************************** Math and rounding ********************************************************************************/
